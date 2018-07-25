e0d585fc4cd0c928c7d8d7225ac60a170601a192
Mikko Miranda <MikkoMiranda@Apples-MacBook-Pro.local>
2018-07-24 19:56:06 -0700

    starter files connected

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// let navBar = document.getElementById("navbar");
+// console.log(navBar);
+
+//  $(document).ready(function(){
+//      $('.menu').click(function(){
+//         $('ul').toggleClass('active');
+//      });
+//  });
