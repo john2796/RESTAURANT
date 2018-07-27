6641ce7a4b78e16af186f7629ac6ea8334095c84
Mikko Miranda <MikkoMiranda@Apples-MacBook-Pro.local>
2018-07-26 19:37:57 -0700

    section one halfway done

--- src/script.js
+++ src/script.js
@@ -6,3 +6,10 @@
 //         $('ul').toggleClass('active');
 //      });
 //  });
+$('.menu-toggle').click(function () {
+  $('.mobile-site-nav').toggleClass('site-nav--open', 500);
+})
+
+$('.menu-toggle').click(function () {
+  $('.hamburger').toggleClass('changeHamburgerStyle');
+})
