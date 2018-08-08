be882cf58279303ed4a754d6780bfc21344c69a5
Mikko Miranda <MikkoMiranda@Apples-MacBook-Pro.local>
2018-08-07 22:13:41 -0700

    123

--- src/script.js
+++ src/script.js
@@ -8,8 +8,19 @@
 //  });
 $('.menu-toggle').click(function () {
   $('.mobile-site-nav').toggleClass('site-nav--open', 500);
-})
+});
 
 $('.menu-toggle').click(function () {
   $('.hamburger').toggleClass('changeHamburgerStyle');
-})
+});
+
+// $(document).ready(function() {
+//   jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000  // 2 seconds
+// });
+
+// Desktop Hover Effect
+$('.desktop-sub-menu li').hover(function(){
+  $(this).children('ul').slideToggle(400);
+});
+
+
